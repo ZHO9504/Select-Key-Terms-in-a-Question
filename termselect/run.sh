9ac73d3fce376473b3e85f3d5e08dbffadbfff2e
ZHO9504 <hozhangel@126.com>
2019-08-28 14:26:25 +0800

    Create run.sh

---
+++ termselect/run.sh
@@ -0,0 +1,4 @@
+
+python3.5 -u src/main.py term --lr 0.02 --dropout_rnn_output 0.4 --batch_size 32 --hidden_size 100  --cat_num 3 --l2_strength 3e-6 --tri_input NA --p_channel True --q_channel True --c_channel True --use_cuda False
+
+Epoch: 12 best_dev_acc: 0.938411  best_test_acc: 0.930641
